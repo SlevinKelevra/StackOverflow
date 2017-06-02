43b524bb426cf51cd3cdc14abeda6b5ef194679c
SlevinKelevra <poztalion2@yandex.ru>
2017-06-02 14:19:04 +0300

    Add test and action for Answer

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create :question }
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves new answer in database' do
+        params = { answer: attributes_for(:answer), question_id: question }
+        expect { post :create, params: params }.to change(question.answers, :count).by(1)
+      end
+      it 'redirects to show view' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        params = { answer: attributes_for(:invalid_answer), question_id: question }
+        expect { post :create, params: params }.to_not change(Answer, :count)
+      end
+      it 're-renders new view' do
+        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }
+        expect(response).to render_template :new
+      end
+    end
+  end
 
 end

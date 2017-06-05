c7b408f20301503a03c5a55b5fc9a0612bce73bb
SlevinKelevra <poztalion2@yandex.ru>
2017-06-05 22:24:01 +0300

    Add sign in users

--- spec/support/acceptance_macros.rb
+++ spec/support/acceptance_macros.rb
@@ -1,4 +1,11 @@
 module AcceptanceMacros
+  def sign_in(user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+  end
+
   def list_questions(questions)
     visit questions_path
     questions.each do |q|

1088ee2114b2e541879b5a99601afabd4d71d122
SlevinKelevra <poztalion2@yandex.ru>
2017-06-06 22:57:15 +0300

    Add create answer for question

--- spec/acceptance/create_answer_for_question_spec.rb
+++ spec/acceptance/create_answer_for_question_spec.rb
@@ -5,6 +5,32 @@ In order to create the answer for the question
 As a user
 I have the opportunity
 } do
+  given(:user) { create :user }
   given(:question) { create :question }
+
+  scenario 'Authenticated user creates answer' do
+    sign_in(user)
+    visit question_path(question)
+
+    fill_in 'Input your answer', with: "answer_body"
+    click_on 'Post your answer'
+
+    expect(page).to have_content "answer_body"
+  end
+
+  scenario 'Non-authenticated user creates answer' do
+    visit question_path(question)
+    click_on 'Post your answer'
+
+    expect(page).to have_content 'need to sign in'
+  end
+
+  scenario 'Authenticated user creates non-valid answer' do
+    sign_in(user)
+    visit question_path(question)
+    click_on 'Post your answer'
+    save_and_open_page
+    expect(page).to have_content "Body can't be blank"
+  end
 
 end

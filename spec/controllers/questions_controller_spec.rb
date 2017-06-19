e3ee45d603b9b6b2bd0ac7bdd02ebdc6767e93fb
SlevinKelevra <poztalion2@yandex.ru>
2017-06-20 00:11:13 +0300

    Add files to answer

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -20,9 +20,15 @@ RSpec.describe QuestionsController, type: :controller do
     it 'assigns requested question to @question' do
       expect(assigns(:question)).to eq question
     end
+
+    it 'builds new attachment for answer' do
+      expect(assigns(:answer).attachments.first).to be_a_new(Attachment)
+    end
+
     it 'assignes new answer to question' do
       expect(assigns(:answer)).to be_a_new(Answer)
     end
+
     it 'renders show view' do
       expect(response).to render_template :show
     end
